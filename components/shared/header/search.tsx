d9f9ba9f1cc622ef1d5df327c3aa26d649465fd4
Somesh Raj <someshraj78569@gmail.com>
2025-04-21 22:54:02 +0530

    Created Search Page

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -2,11 +2,11 @@ import { SearchIcon } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { APP_NAME } from "@/lib/constants"
+import { getAllCategories } from "@/lib/actions/product.action"
 
 
-
-const categories = ['men', 'women', 'kids', 'accessories']
 export default async function Search() {
+    const categories = await getAllCategories()
     return (
         <form action='/search' method='GET' className='flex items-stretch h-10 '>
             <Select name='category'>
@@ -30,7 +30,7 @@ export default async function Search() {
             />
             <button
                 type='submit'
-                className='bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-2 '
+                className='bg-primary text-primary-foreground rounded-s-none rounded-e-md h-full px-3 py-2 '
             >
                 <SearchIcon className='w-6 h-6' />
             </button>
